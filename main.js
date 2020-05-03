288e7e57b34c7b256257354edf2e50676cd7f028
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-05-03 23:07:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var standard, metric,num2,result ,display;
+var standardWeight, standardHeight, metricWeight, metricHeight;
+
+  standard = document.getElementById('first');
+  metric = document.getElementById('second');
+
+  display = document.getElementById('display')
+
+  function displayM() {
+    metric.style.display = "none"
+    standard.style.display = "block"
+  }
+  function display2() {
+    standard.style.display = "none"
+    metric.style.display = 'block'
+  }
+
+  function standardCalc(standardWeight, standardHeight) {
+    standardWeight = document.getElementById('Sweight').value;
+    standardHeight = document.getElementById('Sheight').value;
+
+    result = standardWeight/Math.pow((standardHeight/100),2);
+    num2 = result.toFixed(2)
+   display.innerHTML = parseFloat(num2);
+  }
+
+  function metricCalc(metricWeight, metricHeight) {
+    metricWeight = document.getElementById('Mweight').value;
+    metricHeight = document.getElementById('Mheight').value;
+
+    result = 703 * metricWeight/Math.pow(metricHeight,2);
+    num2 = result.toFixed(2)
+   display.innerHTML = parseFloat(num2);
+  }
