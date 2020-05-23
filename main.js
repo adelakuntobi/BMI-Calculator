1a90a9f5299e330d8c2ac76d235525f4c3d8239a
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-05-23 20:30:36 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var standard, metric,num2,result ,display;
 var standardWeight, standardHeight, metricWeight, metricHeight;
-
+//Assigning the values of the elements gotten from HTML to a JS element
   standard = document.getElementById('first');
   metric = document.getElementById('second');
 
@@ -10,21 +10,24 @@ var standardWeight, standardHeight, metricWeight, metricHeight;
     metric.style.display = "none"
     standard.style.display = "block"
   }
+  //Removes the KG - Cm Coverter
   function display2() {
     standard.style.display = "none"
     metric.style.display = 'block'
   }
 
+  //Function for the KG- CM converter
   function standardCalc(standardWeight, standardHeight) {
+    //Gets the user input
     standardWeight = document.getElementById('Sweight').value;
     standardHeight = document.getElementById('Sheight').value;
 
     result = standardWeight/Math.pow((standardHeight/100),2);
-    num2 = result.toFixed(2)
+    num2 = result.toFixed(2)//Makes it at most 2 decimal places
     if (result == NaN){
-      display.innerHTML = "Error";
+      display.innerHTML = "Invalid";
     }
-    else if(result == Infinity){display.innerHTML = "Error";}
+    else if(result == Infinity){display.innerHTML = "Invalid ";}
    else{display.innerHTML = parseFloat(num2);}
   }
 
@@ -33,11 +36,11 @@ var standardWeight, standardHeight, metricWeight, metricHeight;
     metricHeight = document.getElementById('Mheight').value;
 
     result = 703 * metricWeight/Math.pow(metricHeight,2);
-    num2 = result.toFixed(2)
-    if (result == NaN){
-      display.innerHTML = "Error";
+    num2 = result.toFixed(2)//Makes it at most 2 decimal places
+    if (result == NaN){//If the result is not a number
+      display.innerHTML = "Invalid";
     }
-    else if(result == Infinity){display.innerHTML = "Error";}
+    else if(result == Infinity){display.innerHTML = "Invalid ";}
    else{display.innerHTML = parseFloat(num2);}
 
   }
